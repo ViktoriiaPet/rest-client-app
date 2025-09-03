2bb3af6bb9ac02b1c55a368fdbb25a0cd093fb30
Viktoriia Pet <viktoriiap0205199@gmail.com>
2025-09-03 20:33:49 +0200

    fix: update distance between srings

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,12 +4,14 @@ export default function HomePage() {
   return (
     <div className="p-[5vw]">
       <div>Start Page (Welcome)</div>
+        <div className='flex flex-row gap-[5vw]'>
       <NavLink to="/singIn" end>
         <button>Sing In</button>
       </NavLink>
       <NavLink to="/singUp" end>
         <button>Sing Up</button>
       </NavLink>
+      </div>
     </div>
   );
 }
