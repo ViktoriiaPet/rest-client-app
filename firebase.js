287ef1ce5096355521db68c24867bd02c411af1c
Darya Daniuk <darina.daniuk@gmail.com>
2025-09-03 21:47:36 -0400

    fix: fix whitespace error

--- firebase.js
+++ firebase.js
@@ -1,29 +1,23 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 import {
   getAuth,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   sendPasswordResetEmail,
   signOut,
-} from "firebase/auth";
-import {
-  getFirestore,
-  collection,
-  addDoc,
-​​} from "firebase/firestore";
-
+} from 'firebase/auth';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAFMdW1Th1OKcBvFuxokKb8GAzHtyxU6Wk",
-  authDomain: "rest-client-app-5769b.firebaseapp.com",
-  projectId: "rest-client-app-5769b",
-  storageBucket: "rest-client-app-5769b.firebasestorage.app",
-  messagingSenderId: "871751415386",
-  appId: "1:871751415386:web:5e24636a4e7fbf9a640a63",
-  measurementId: "G-H091NY0HYV"
+  apiKey: 'AIzaSyAFMdW1Th1OKcBvFuxokKb8GAzHtyxU6Wk',
+  authDomain: 'rest-client-app-5769b.firebaseapp.com',
+  projectId: 'rest-client-app-5769b',
+  storageBucket: 'rest-client-app-5769b.firebasestorage.app',
+  messagingSenderId: '871751415386',
+  appId: '1:871751415386:web:5e24636a4e7fbf9a640a63',
+  measurementId: 'G-H091NY0HYV',
 };
-
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
@@ -43,10 +37,10 @@ const registerWithEmailAndPassword = async (name, email, password) => {
   try {
     const res = await createUserWithEmailAndPassword(auth, email, password);
     const user = res.user;
-    await addDoc(collection(db, "users"), {
+    await addDoc(collection(db, 'users'), {
       uid: user.uid,
       name,
-      authProvider: "local",
+      authProvider: 'local',
       email,
     });
   } catch (err) {
@@ -58,7 +52,7 @@ const registerWithEmailAndPassword = async (name, email, password) => {
 const sendPasswordReset = async (email) => {
   try {
     await sendPasswordResetEmail(auth, email);
-    alert("Password reset link sent!");
+    alert('Password reset link sent!');
   } catch (err) {
     console.error(err);
     alert(err.message);
@@ -76,4 +70,4 @@ export {
   registerWithEmailAndPassword,
   sendPasswordReset,
   logout,
-}
+};
