782a29a2d459225c6493f9437be8ae60631d6521
alyona317 <oschepkova.alyona@yandex.ru>
2025-09-06 18:02:26 +0300

    feat: add buttons to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { NavLink } from 'react-router';
 
+import { Button } from '@/components/ui/button';
+
 import LangToggle from './LangToggle';
 
 import type { JSX } from 'react';
@@ -17,12 +19,25 @@ export default function Header(): JSX.Element {
       <div>
         <LangToggle />
       </div>
-      <NavLink to="/singIn" end>
-        <div>{t('SignIn')}</div>
-      </NavLink>
-      <NavLink to="/singUp" end>
-        <div>{t('SignUp')}</div>
-      </NavLink>
+      <div className="flex flex-row justify-around ">
+        <NavLink to="/singIn" end>
+          <Button
+            variant="outline"
+            className="bg-rose-400 hover:bg-rose-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md"
+          >
+            {t('SignIn')}
+          </Button>
+        </NavLink>
+        <NavLink to="/singUp" end>
+          <Button
+            variant="outline"
+            className="bg-rose-400 hover:bg-rose-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md"
+          >
+            {t('SignUp')}
+          </Button>
+        </NavLink>
+      </div>
+
       <NavLink to="/mainClint" end>
         <div>Authorized user&apos;s page</div>
       </NavLink>
