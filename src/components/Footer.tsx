de183313ff486a05a407e2bcb35a18e72db3275d
darinadaniuk <darina.daniuk@gmail.com>
2025-09-06 21:55:23 -0400

    feat: change footer (#12)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { useTranslation } from 'react-i18next';
 
 import type { JSX } from 'react';
 
 export default function Footer(): JSX.Element {
+  const { t } = useTranslation();
+  const year = new Date().getFullYear();
+
   return (
-    <div className="flex flex-row justify-around fixed bottom-0 left-0 w-full ">
-      <Link to="https://github.com/ViktoriiaPet/rest-client-app">
-        <div>GitHub</div>
-      </Link>
-      <div>2025</div>
-      <Link to="https://rs.school/">
-        <div>Logo</div>
-      </Link>
-    </div>
+    <footer className="sticky bottom-0 w-full">
+      <div className="mx-auto max-w-5xl px-4 py-3 flex items-center justify-between">
+        <a
+          href="https://github.com/ViktoriiaPet/rest-client-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {t('Github')}
+        </a>
+
+        <span>{year}</span>
+
+        <a href="https://rs.school/" target="_blank" rel="noreferrer">
+          Logo
+        </a>
+      </div>
+    </footer>
   );
 }
