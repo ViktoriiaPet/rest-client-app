da0fd44d57e81a1049f4289e007e49fed8f1ca5f
alyona317 <oschepkova.alyona@yandex.ru>
2025-09-08 21:44:52 +0300

    refactor: change alertDialog component to Dialog component

--- src/components/ErrorModal.tsx
+++ src/components/ErrorModal.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 
 import type { JSX } from 'react';
-
 import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-} from '@/components/ui/alert-dialog';
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+
 import { getStatusColor } from '@/utils/getStatusColor';
 
 type ErrorModalProps = {
@@ -29,20 +26,19 @@ export const ErrorModal = ({
 }: ErrorModalProps): JSX.Element => {
   const bgColorClass = getStatusColor(statusCode);
   return (
-    <AlertDialog open={isOpen} onOpenChange={onClose}>
-      <AlertDialogContent className={bgColorClass}>
-        <AlertDialogHeader>
-          <AlertDialogTitle className="text-white">
-            Error {statusCode || 'unknown'}
-          </AlertDialogTitle>
-          <AlertDialogDescription className="text-white">
-            {message || 'Произошла ошибка при выполнении запроса.'}
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel onClick={onClose}>Close</AlertDialogCancel>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+    <>
+      <Dialog open={isOpen} onOpenChange={onClose}>
+        <DialogContent className={bgColorClass}>
+          <DialogHeader>
+            <DialogTitle className="text-white">
+              Error {statusCode || 'unknown'}
+            </DialogTitle>
+            <DialogDescription className="text-white">
+              {message || 'Произошла ошибка при выполнении запроса.'}
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
