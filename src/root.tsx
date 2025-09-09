8938514e3c0ede40873d458cc30724b6f58825a9
darinadaniuk <darina.daniuk@gmail.com>
2025-09-08 21:16:41 -0400

    feat: add error boundary (#9)

--- src/root.tsx
+++ src/root.tsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { Outlet, Scripts } from 'react-router';
 
 import './i18n/i18n.ts';
+import ErrorBoundary from './components/ErrorBoundary';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import { store } from './store';
@@ -25,9 +26,11 @@ export default function Root(): JSX.Element {
               <div className="p-[2vw] font-inter text-xl text-purple-600 ">
                 Postman API
               </div>
-              <div className=" p-[2vw] w-11/12 max-w-4xl rounded-[10%] bg-pink-200 flex flex-col items-center">
-                <Outlet />
-              </div>
+              <ErrorBoundary>
+                <div className=" p-[2vw] w-11/12 max-w-4xl rounded-[10%] bg-pink-200 flex flex-col items-center">
+                  <Outlet />
+                </div>
+              </ErrorBoundary>
             </main>
             <Scripts />
             <Footer />
