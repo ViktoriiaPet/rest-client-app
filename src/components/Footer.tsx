ae716688443b9e8fc9f81b91a9732a91aecf1469
alyona317 <oschepkova.alyona@yandex.ru>
2025-09-15 21:43:36 +0300

    fix: delete sticky from footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,20 +6,25 @@ import type { JSX } from 'react';
 export default function Footer(): JSX.Element {
   const { t } = useTranslation();
   return (
-    <footer className="sticky bottom-0 w-full">
+    <footer className="bottom-0 w-full">
       <div className="mx-auto max-w-5xl px-4 py-3 flex items-center justify-between">
         <a
           href="https://github.com/ViktoriiaPet/rest-client-app"
           target="_blank"
           rel="noreferrer"
+          className="text-purple-600 "
         >
           {t('Github')}
         </a>
 
-        <span>2025</span>
+        <span className="text-purple-600">2025</span>
 
         <a href="https://rs.school/" target="_blank" rel="noreferrer">
-          Logo
+          <img
+            className="w-[40px]"
+            src="https://rs.school/_next/static/media/rss-logo.c19ce1b4.svg"
+            alt="rss logo"
+          />
         </a>
       </div>
     </footer>
