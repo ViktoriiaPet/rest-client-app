a63de38351296d6566bdb28180878aedd90e7b01
alyona317 <oschepkova.alyona@yandex.ru>
2025-09-15 23:00:30 +0300

    feat: add info about alyona

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -58,59 +58,57 @@ export default function Header(): JSX.Element | null {
     }
   };
 
-
   if (!ready) return null;
 
   return (
     <header className="sticky top-0 z-50 w-full">
-        <div
-          className={
-            `sticky top-0  py-2 z-50 w-full backdrop-blur-sm  px-4 py-2 ` +
-            `${reduceMotion ? '' : 'transition-all duration-300 ease-in-out'} ` +
-            (scrolled
-              ? 'bg-white/80 border-b border-pink-300 shadow-md'
-              : 'bg-transparent border-b-0')
-          }
-        >
-          <nav className="flex flex-row justify-between items-center">
-            <NavLink to="/" end>
-              <img src="/app-logo.svg" width="60" height="60" />
-            </NavLink>
-            <div className="w-[300px] flex items-center justify-center">
-              <LangToggle />
+      <div
+        className={
+          `sticky top-0  py-2 z-50 w-full backdrop-blur-sm  px-4 py-2 ` +
+          `${reduceMotion ? '' : 'transition-all duration-300 ease-in-out'} ` +
+          (scrolled
+            ? 'bg-white/80 border-b border-pink-300 shadow-md'
+            : 'bg-transparent border-b-0')
+        }
+      >
+        <nav className="flex flex-row justify-between items-center">
+          <NavLink to="/" end>
+            <img src="/app-logo.svg" width="60" height="60" />
+          </NavLink>
+          <div className="w-[300px] flex items-center justify-center">
+            <LangToggle />
+          </div>
+          <div className="flex flex-row items-center justify-center  w-[200px]"></div>
+          {user ? (
+            <>
+              <Button variant="custom" onClick={handleLogout}>
+                {t('LogOut')}
+              </Button>
+              <NavLink to="/mainClint" end>
+                <Button variant="custom">{t('MainPage')}</Button>
+              </NavLink>
+              <NavLink to="/mainClint" end>
+                <div className="w-1/4">
+                  <Avatar>
+                    <AvatarImage src={userIcon} />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                  <span>{user.displayName}</span>
+                </div>
+              </NavLink>
+            </>
+          ) : (
+            <div className="flex gap-2">
+              <NavLink to="/signIn" end className="flex justify-end">
+                <SignButton text={t('auth.signIn')} />
+              </NavLink>
+              <NavLink to="/signUp" end>
+                <SignButton text={t('auth.signUp')} />
+              </NavLink>
             </div>
-            <div className="flex flex-row items-center justify-center  w-[200px]"></div>
-            {user ? (
-              <>
-                <Button variant="custom" onClick={handleLogout}>
-                  {t('LogOut')}
-                </Button>
-                <NavLink to="/mainClint" end>
-                  <Button variant="custom">{t('MainPage')}</Button>
-                </NavLink>
-                <NavLink to="/mainClint" end>
-                  <div className="w-1/4">
-                    <Avatar>
-                      <AvatarImage src={userIcon} />
-                      <AvatarFallback>CN</AvatarFallback>
-                    </Avatar>
-                    <span>{user.displayName}</span>
-                  </div>
-                </NavLink>
-              </>
-            ) : (
-              <div className="flex gap-2">
-                <NavLink to="/signIn" end className="flex justify-end">
-                  <SignButton text={t('auth.signIn')} />
-                </NavLink>
-                <NavLink to="/signUp" end>
-                  <SignButton text={t('auth.signUp')} />
-                </NavLink>
-              </div>
-            )}
-          </nav>
-        </div>
-
+          )}
+        </nav>
+      </div>
     </header>
   );
 }
