7654f2594d6876e03381696b8e4e370c724f800e
alyona317 <oschepkova.alyona@yandex.ru>
2025-09-20 00:01:25 +0300

    feat: add footer test

--- src/test/languageSlice.test.ts
+++ src/test/languageSlice.test.ts
@@ -2,7 +2,7 @@ import { describe, it, expect, beforeEach } from 'vitest';
 import reducer, { setLanguage } from '@/store/languageSlice';
 
 describe('languageSlice', () => {
-  const initialState: { lang: "en" | "ru" | null } = { lang: 'en' };
+  const initialState: { lang: 'en' | 'ru' | null } = { lang: 'en' };
 
   beforeEach(() => {
     localStorage.clear();
@@ -20,7 +20,10 @@ describe('languageSlice', () => {
   });
 
   it('should update language from en to ru', () => {
-    let state = reducer({ lang: 'en' as "en" | "ru" | null }, setLanguage('ru'));
+    let state = reducer(
+      { lang: 'en' as 'en' | 'ru' | null },
+      setLanguage('ru')
+    );
     expect(state.lang).toBe('ru');
     expect(localStorage.getItem('i18nextLng')).toBe('ru');
 
@@ -28,5 +31,4 @@ describe('languageSlice', () => {
     expect(state.lang).toBe('en');
     expect(localStorage.getItem('i18nextLng')).toBe('en');
   });
-
 });
