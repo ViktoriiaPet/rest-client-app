8ab0e6e0cc8888bb56fe46a3288959074b6a44e0
darinadaniuk <darina.daniuk@gmail.com>
2025-09-21 13:51:41 -0400

    fix: add some styles (#38)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
+import type { JSX } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useState } from 'react';
 import { useEffect } from 'react';
-
-import type { JSX } from 'react';
+import rssLogo from '../../public/rss-logo.svg';
 
 export default function Footer(): JSX.Element | null {
   const [mounted, setMounted] = useState(false);
@@ -12,25 +13,27 @@ export default function Footer(): JSX.Element | null {
   }, []);
   if (!mounted) return null;
   return (
-    <footer className="bottom-0 w-full bg-pink-200 px-4 py-2">
-      <div className="mx-auto max-w-5xl flex items-center justify-between">
+    <footer className="bottom-0 w-full bg-pink-200 px-8 py-2">
+      <div className="mx-auto w-full  flex items-center justify-between overflow-y-auto pt-0">
         <a
           href="https://github.com/ViktoriiaPet/rest-client-app"
           target="_blank"
           rel="noreferrer"
-          className="text-purple-600 "
+          className="text-purple-600 font-bold text-center text-l mb-2 mt-2  hover:text-yellow-100 transition-colors"
         >
           {t('app.github')}
         </a>
 
-        <span className="text-purple-600">2025</span>
+        <span className="text-purple-600 font-bold text-center text-l mb-2 mt-2">
+          2025
+        </span>
 
-        <a href="https://rs.school/" target="_blank" rel="noreferrer">
-          <img
-            className="w-[40px]"
-            src="https://rs.school/_next/static/media/rss-logo.c19ce1b4.svg"
-            alt="rss logo"
-          />
+        <a
+          href="https://rs.school/courses/reactjs"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img className="w-[40px]" src={rssLogo} alt="rss logo" />
         </a>
       </div>
     </footer>
